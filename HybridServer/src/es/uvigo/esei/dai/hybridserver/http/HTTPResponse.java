62a62d4b6a7918e7f9ae1b129108df666bca477f
Santiago Gómez <santii810@hotmail.com>
2018-10-16 12:34:50 +0200

    Update HybridServer/src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/http/HTTPResponse.java
@@ -1,68 +1,105 @@
 package es.uvigo.esei.dai.hybridserver.http;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class HTTPResponse {
+
+	private HTTPResponseStatus status;
+	private String version;
+	private String content;
+	private Map<String, String> parameters;
+
 	public HTTPResponse() {
+		this.parameters = new LinkedHashMap<String, String>();
+
 	}
 
 	public HTTPResponseStatus getStatus() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.status;
 	}
 
 	public void setStatus(HTTPResponseStatus status) {
+		this.status = status;
 	}
 
 	public String getVersion() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.version;
 	}
 
 	public void setVersion(String version) {
+		this.version = version;
 	}
 
 	public String getContent() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.content;
 	}
 
 	public void setContent(String content) {
+		this.content = content;
 	}
 
 	public Map<String, String> getParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.parameters;
 	}
 
 	public String putParameter(String name, String value) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.parameters.put(name, value);
 	}
 
 	public boolean containsParameter(String name) {
-		// TODO Auto-generated method stub
-		return false;
+		return parameters.containsKey(name);
 	}
 
 	public String removeParameter(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.parameters.remove(name);
 	}
 
 	public void clearParameters() {
+		this.parameters.clear();
 	}
 
 	public List<String> listParameters() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> toret = new LinkedList<String>();
+		for (String key : parameters.keySet()) {
+			toret.add(key + ": " + parameters.get(key) + "\r\n");
+		}
+		return toret;
 	}
 
 	public void print(Writer writer) throws IOException {
+		BufferedWriter bw = new BufferedWriter(writer);
+		StringBuilder message = new StringBuilder(this.getVersion());
+		message.append(" ");
+		message.append(this.getStatus().getCode());
+		message.append(" ");
+		message.append(this.status.getStatus());
+		message.append("\r\n");
+
+		if (!this.getParameters().isEmpty()) {
+			for (String parameter : listParameters()) {
+				message.append(parameter);
+			}
+		}
+
+		if (this.getContent() != null) {
+			message.append("Content-Length: ");
+			message.append(this.getContent().length());
+			message.append("\r\n\r\n");
+			message.append(this.getContent());
+		} else {
+			message.append("\r\n");
+		}
+
+		bw.write(message.toString());
+		bw.flush();
+
 	}
 
 	@Override
